2ddd419093441666eba4de5912c23ca99b10de90
Jayden12234 <130702014+Jayden12234@users.noreply.github.com>
2023-11-24 13:55:40 +0000

    new

--- README.md
+++
@@ -1,2 +0,0 @@
-to run the q# code from Qrng.qs run dotnet run
-it executes from the operation main 
---
+++ runner.sh
@@ -0,0 +1,7 @@
+git clone https://github.com/microsoft/Quantum.git
+mv /workspaces/quantum-compiler/Quantum/samples/interoperability/qrng/Host.cs /workspaces/quantum-compiler
+mv /workspaces/quantum-compiler/Quantum/samples/interoperability/qrng/host.py /workspaces/quantum-compiler
+mv /workspaces/quantum-compiler/Quantum/samples/interoperability/qrng/Qrng.csproj /workspaces/quantum-compiler
+mv /workspaces/quantum-compiler/Quantum/samples/interoperability/qrng/Qrng.qs /workspaces/quantum-compiler
+mv /workspaces/quantum-compiler/Quantum/samples/interoperability/qrng/README.md /workspaces/quantum-compiler
+rm -r /workspaces/quantum-compiler/Quantum
