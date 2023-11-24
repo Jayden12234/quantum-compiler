c62b8946b2887fbc727b10e1eda952944d29b90a
Jayden12234 <130702014+Jayden12234@users.noreply.github.com>
2023-11-24 14:30:20 +0000

    Create runner.sh

---
+++ runner.sh
@@ -0,0 +1,5 @@
+cd /workspaces/quantum-compiler/
+rm -r qrng
+git clone https://github.com/microsoft/Quantum.git
+mv /workspaces/quantum-compiler/Quantum/samples/interoperability/qrng /workspaces/quantum-compiler/
+rm -r Quantum
