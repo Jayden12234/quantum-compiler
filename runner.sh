20d76cc24e5d96e827c8f7b0b24546cfd867d456
Jayden12234 <130702014+Jayden12234@users.noreply.github.com>
2024-09-26 11:45:47 +0100

    Update runner.sh

--- runner.sh
+++ runner.sh
@@ -1,8 +1,4 @@
-cd /workspaces/quantum-compiler/
-rm -r qrng
-git clone https://github.com/microsoft/Quantum.git
-mv /workspaces/quantum-compiler/Quantum/samples/interoperability/qrng /workspaces/quantum-compiler/
-rm -r Quantum
+
 wget https://packages.microsoft.com/config/ubuntu/$(lsb_release -rs)/packages-microsoft-prod.deb -O packages-microsoft-prod.deb
 sudo dpkg -i packages-microsoft-prod.deb
 
@@ -11,8 +7,4 @@ sudo apt-get install -y apt-transport-https
 sudo apt-get update
 sudo apt-get install -y dotnet-sdk-6.0
 
-rm /workspaces/quantum-compiler/qrng/Host.cs
-rm /workspaces/quantum-compiler/qrng/Qrng.qs
-mv /workspaces/quantum-compiler/Host.cs /workspaces/quantum-compiler/qrng/
-mv /workspaces/quantum-compiler/Qrng.qs /workspaces/quantum-compiler/qrng/
 
